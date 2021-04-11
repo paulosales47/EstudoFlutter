103dc48c58226da8ff0d71e2d48329d8fbc0a6fa
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2021-04-11 10:19:20 -0300

    Aula 60 - Espaçamentos

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,26 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
     title: "App Estudo",
+    debugShowCheckedModeBanner: false,
     home: Container(
-      color: Colors.white,
+      padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+      margin: EdgeInsets.fromLTRB(0, 25, 0, 0),
+      decoration: BoxDecoration(
+        color: Colors.black,
+        border: Border.all(width: 3, color: Colors.white)
+      ),
       child: Column(
         children: [
-          TextButton(
-              onPressed: (){
-                print("TESTE");
-              },
-              child: Text(
-                "Ok",
-                style: TextStyle(
-                    color: Colors.black,
-                    decoration: TextDecoration.none
-                    )
-              )
+          Text("Lorem Ipsum is simply dummy text of the printing and typesetting"
+              " industry.", style: TextStyle(decoration: TextDecoration.none, fontSize: 30),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 25, 0, 25),
+            child:Text("Lorem Ipsum is simply dummy text of the printing and typesetting"
+              " industry.", style: TextStyle(decoration: TextDecoration.none, fontSize: 30),
+          )),
+          Text("Lorem Ipsum is simply dummy text of the printing and typesetting"
+              " industry.", style: TextStyle(decoration: TextDecoration.none, fontSize: 30),
           )
         ],
       ),
