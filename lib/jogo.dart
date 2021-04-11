71f758f48132c5100dcf187a664648dc1333c726
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2021-04-11 18:39:51 -0300

    #App2 JokenPo - Exibindo resultado

--- lib/jogo.dart
+++ lib/jogo.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +11,25 @@ class Jogo extends StatefulWidget {
 class _JogoState extends State<Jogo> {
 
   var _textoResultado = "";
+  var _imagemApp = AssetImage("images/jokenpo/padrao.png");
+
+  void _calcularEscollhaApp(String escolhaUsuario){
+    var _imagens = ["papel", "pedra", "tesoura"];
+    var escolhaApp = _imagens[Random().nextInt(3)];
+
+    if((escolhaApp == "papel" && escolhaUsuario == "tesoura") ||
+       (escolhaApp == "pedra" && escolhaUsuario == "papel")   ||
+       (escolhaApp == "tesoura" && escolhaUsuario == "pedra"))
+      _textoResultado = "Usuário ganhou";
+    else if(escolhaApp  == escolhaUsuario)
+      _textoResultado = "Empate";
+    else
+      _textoResultado = "App Ganhou";
+
+    setState(() {
+      _imagemApp = AssetImage("images/jokenpo/${escolhaApp}.png");
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,24 +46,24 @@ class _JogoState extends State<Jogo> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Text(
-                "Escolha do App",
-                style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold
-                )),
-            GestureDetector(
-              onTap: (){print("clique");},
-              onDoubleTap: (){print("double clique");},
-              onLongPress: (){print("clique longo");},
-              child: Image.asset("images/jokenpo/padrao.png"),
+              "Escolha do App",
+              style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold
+              )),
+            Image(image:_imagemApp),
+            Text(
+              _textoResultado,
+              style: TextStyle(
+                fontSize: 25
+              ),
             ),
-            Text(_textoResultado),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Expanded(child: Image.asset("images/jokenpo/pedra.png")),
-                Expanded(child: Image.asset("images/jokenpo/papel.png")),
-                Expanded(child: Image.asset("images/jokenpo/tesoura.png"))
+                Expanded(child: GestureDetector(child: Image.asset("images/jokenpo/papel.png"), onTap: () => _calcularEscollhaApp("papel"),)),
+                Expanded(child: GestureDetector(child: Image.asset("images/jokenpo/pedra.png"), onTap: () => _calcularEscollhaApp("pedra"),)),
+                Expanded(child: GestureDetector(child: Image.asset("images/jokenpo/tesoura.png"), onTap: () => _calcularEscollhaApp("tesoura"),))
               ],
             )
           ],
