6a205731f275306930de33486faac6d22648a2c8
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2021-04-11 18:05:40 -0300

    #App2 JokenPo - Criando interface

---
+++ lib/jogo.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Jogo extends StatefulWidget {
+  @override
+  _JogoState createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+
+  var _textoResultado = "";
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("JokenPo"),
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(16, 25, 16, 0),
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+                "Escolha do App",
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold
+                )),
+            Image.asset("images/jokenpo/padrao.png"),
+            Text(_textoResultado),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(child: Image.asset("images/jokenpo/pedra.png")),
+                Expanded(child: Image.asset("images/jokenpo/papel.png")),
+                Expanded(child: Image.asset("images/jokenpo/tesoura.png"))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,11 @@
-import 'dart:math';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'jogo.dart';
 
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Home(),
+    home: Jogo(),
   ));
-}
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-
-  var _frases = ["TESTE 1", "TESTE 2", "TESTE 3", "TESTE 4", "TESTE 5"];
-  var _frase = "Clique abaixo para gerar uma frase";
-
-  void _gerarFrase(){
-    setState(() {
-      _frase = _frases[Random().nextInt(_frases.length)];
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Frases do dia"),
-        backgroundColor: Colors.green,
-      ),
-      body: Container(
-        padding: EdgeInsets.all(16),
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Image.asset("images/logo.png"),
-            Text(
-              _frase,
-              textAlign: TextAlign.justify,
-              style: TextStyle(
-                fontSize: 17,
-              ),
-            ),
-            ElevatedButton(
-                onPressed: _gerarFrase,
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(Colors.green)
-                ),
-                child: Text(
-                  "Nova frase",
-                  style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold
-                  ),
-                )
-            )
-          ],
-        ),
-      )
-    );
-  }
 }
