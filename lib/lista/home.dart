feb043f498d0842a0502d98d5ad648c24593854e
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2021-04-18 10:01:14 -0300

    Eventos de clique dentro da lista e utilizando dialog (AlertDialog)

--- lib/lista/home.dart
+++ lib/lista/home.dart
@@ -11,6 +11,8 @@ class _HomeState extends State<Home> {
   List _itens = [];
 
   void _carregarItens(){
+    _itens = [];
+
     for(int i = 0; i <= 10; i++){
       Map<String, dynamic> item = Map();
       item["titulo"] = "Lorem ${i} ipsum dolor sit amet, consectetur";
@@ -35,6 +37,27 @@ class _HomeState extends State<Home> {
           itemBuilder: (context, indice){
             print("Item: ${indice}");
             return ListTile(
+              onTap: (){
+                showDialog(context: context, builder: (context){
+                  return AlertDialog(
+                    backgroundColor: Colors.white,
+                    title: Text(_itens[indice]["titulo"]),
+                    titlePadding: EdgeInsets.all(20),
+                    titleTextStyle: TextStyle(fontSize: 20, color: Colors.orange),
+                    content: Text(_itens[indice]["descricao"]),
+                    contentPadding: EdgeInsets.all(20),
+                    actions: [
+                      TextButton(onPressed: (){
+                        Navigator.pop(context);
+                      }, child: Text("Salvar")),
+                      TextButton(onPressed: (){
+                        Navigator.pop(context);
+                      }, child: Text("Cancelar"))
+                    ],
+                  );
+                });
+              },
+              onLongPress: (){},
               title: Text(_itens[indice]["titulo"]),
               subtitle: Text(_itens[indice]["descricao"]),
             );
