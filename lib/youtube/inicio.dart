1061fff802c265723edd2d1854fa2fb40e215666
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2021-04-19 18:09:37 -0300

    #App7 Youtube - Executando vídeos

--- lib/youtube/inicio.dart
+++ lib/youtube/inicio.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_estudo/youtube/api.dart';
 import 'package:flutter_estudo/youtube/model/video.dart';
+import 'package:flutter_youtube/flutter_youtube.dart';
 
 class Inicio extends StatefulWidget {
 
@@ -15,11 +16,12 @@ class Inicio extends StatefulWidget {
 
 class _InicioState extends State<Inicio> {
 
+
+
   @override
   Widget build(BuildContext context) {
 
     final apiYouTube = ApiYoutube();
-
     Future<List<Video>> _buscarVideos(String termoBusca){
       return apiYouTube.pesquisar(termoBusca);
     }
@@ -34,28 +36,41 @@ class _InicioState extends State<Inicio> {
             separatorBuilder: (context, indice) => Divider(height: 2, color: Colors.black38),
             itemCount: snashot.data.length,
             itemBuilder: (context, indice){
-              return Column(
-                children: [
-                  Container(
-                    height: 200,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: NetworkImage(snashot.data[indice].imagem)
+              return GestureDetector(
+                onTap: (){},
+                child: GestureDetector(
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 200,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(snashot.data[indice].imagem)
+                            )
+                        ),
+                      ),
+                      ListTile(
+                        title: Padding(
+                            padding: EdgeInsets.only(top: 10, bottom: 10),
+                            child: Text(snashot.data[indice].titulo)
+                        ),
+                        subtitle: Padding(
+                            padding: EdgeInsets.only(bottom: 15),
+                            child: Text(snashot.data[indice].descricao)
+                        ),
                       )
-                    ),
+                    ],
                   ),
-                  ListTile(
-                    title: Padding(
-                      padding: EdgeInsets.only(top: 10, bottom: 10),
-                      child: Text(snashot.data[indice].titulo)
-                    ),
-                    subtitle: Padding(
-                        padding: EdgeInsets.only(bottom: 15),
-                        child: Text(snashot.data[indice].descricao)
-                    ),
-                  )
-                ],
+                  onTap: (){
+                    FlutterYoutube.playYoutubeVideoById(
+                      apiKey: CHAVE_YOUTUBE_API,
+                      videoId: snashot.data[indice].id,
+                      autoPlay: true,
+                      fullScreen: true
+                    );
+                  },
+                ),
               );
             });
         }
